441d3a92e767a8f0f0bc7f8817df0a03a0807c28
Inf1nityy <br0wn_rabbit@protonmail.com>
2023-10-17 19:25:46 +0200

    cleaning up

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 from board import Board
 
 pygame.init()
-checkmate = False
 
 if __name__ == "__main__":
     board = Board(600, 600)
@@ -15,14 +14,9 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if checkmate is False:
+                if board.is_in_checkmate('black') == False and board.is_in_checkmate('white') == False:
                     if event.button == 1:
                         board.handle_click(mx, my)
-
-        if board.is_in_checkmate('black'): # If black is in checkmate
-            checkmate = True
-        elif board.is_in_checkmate('white'): # If white is in checkmate
-            checkmate = True
 
         board.draw_board()
         pygame.display.update()
