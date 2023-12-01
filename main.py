3b17ca29340baaf58336c3e31e77ce3d2dc5408c
Inf1nityy <br0wn_rabbit@protonmail.com>
2023-12-01 18:52:24 +0100

    changing depth value

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Chess")
 if __name__ == "__main__":
     board = Board(600, 600)
 
-    result = board.number_of_possible_positions(depth=4)
+    result = board.number_of_possible_positions(depth=2)
     print(f"Number of possible positions: {result}")
 
     while True:
